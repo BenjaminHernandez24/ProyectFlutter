28914df37c0f0c20c3d9734e63a04eeb7e704e58
NoeProfesional2000 <Noeprofesional2000@gmail.com>
2021-11-22 18:06:28 -0600

    Linea 18

--- lib/ver_datos.dart
+++ lib/ver_datos.dart
@@ -16,7 +16,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const _MyList(title: 'Lista Censos'),
+      home: new Scaffold(
+        body: _MyList(title: 'Lista Censos'),
+      ),
     );
   }
 }
